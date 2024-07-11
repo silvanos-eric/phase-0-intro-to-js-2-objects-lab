96d2b0b0537de72c86575c5e938bf4cab23e5ee1
Silvanos Eric <ericoderito@gmail.com>
2024-07-11 13:03:16 +0300

    feat: implement employee object manipulation functions
    Add functions to update and delete employee key-value pairs both immutably and destructively. These
    include creating new employee objects with updated values and directly modifying the original
    employee object.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+  name: "John Doe",
+  streetAddress: "Microsoft",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  const employeeClone = { ...employee };
+  employeeClone[key] = value;
+  return employeeClone;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+  const employeeClone = { ...employee };
+  delete employeeClone[key];
+  return employeeClone;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
